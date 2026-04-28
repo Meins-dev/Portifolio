8d5add1be93b9fcd4ef021420714c1b3475bd31b
Meins-dev <meinspaulo@gmail.com>
2026-04-28 14:13:12 -0400

    programa que continua o codigo dentro do while

---
+++ continuar.py
@@ -0,0 +1,7 @@
+while True:
+    valor=int(input("Digite 1 ou 0 pra encerrar"))
+    if valor <=1 :
+        continue
+    print("numero maior que 1")
+    if valor > 1 :
+        print("numero maior que 1")
